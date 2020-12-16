157983b727509758c4dd9da11558198dfedb845f
DarianneSalinas <darianne.salinas@gmail.com>
2020-12-16 15:38:37 -0700

    Testing app launch screen

--- 12-16-20 Launch Screen Testing/ViewController.swift
+++ 12-16-20 Launch Screen Testing/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+   
+    let pizzaImage = UIImageView(image: UIImage(named: "pizza"))
+    let splashView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+//        splashView.backgroundColor = UIColor(red: 255/255, green: 53/255, blue: 79/2, alpha: 1.0)
+        splashView.backgroundColor = .purple
+        view.addSubview(splashView)
+        
+        splashView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        
+        pizzaImage.contentMode = .scaleAspectFit
+        splashView.addSubview(pizzaImage)
+        pizzaImage.frame = CGRect(x: splashView.frame.midX - 50, y: splashView.frame.midY - 50, width: 100, height: 100)
+        
     }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+            self.scaleDownAnimation()
+        }
+    }
+    func scaleDownAnimation() { //1. start of animation
+        UIView.animate(withDuration: 0.5, animations: {
+            self.pizzaImage.transform = CGAffineTransform(scaleX: 0.5, y: 0.5 )
+            })
+        { (success) in
+            self.scaleUpAnimation() //2. after the start, it goes here
+        }
 
+}
+
+    func scaleUpAnimation() {
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: .curveEaseIn,
+                       animations: {
+                        self.pizzaImage.transform = CGAffineTransform(scaleX: 5, y: 5)
+                        
+        } ) { (success) in
+            self.removeSplashScreen()
+        }
+
+    }
+    func removeSplashScreen() {
+        splashView.removeFromSuperview()
+    }
 }
 
