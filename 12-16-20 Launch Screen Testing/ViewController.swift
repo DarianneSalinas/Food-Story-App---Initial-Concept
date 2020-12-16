4a21518e27f99b887b14464a8d93b02985e8dbba
DarianneSalinas <darianne.salinas@gmail.com>
2020-12-16 01:42:04 -0700

    Initial Commit

---
+++ 12-16-20 Launch Screen Testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  12-16-20 Launch Screen Testing
+//
+//  Created by Darianne Salinas on 12/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
